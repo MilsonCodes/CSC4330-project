84bf1436c95d95bf2fc304906b2ac535df418378
milsoncodes <mr.sheppii@gmail.com>
2020-03-08 19:55:54 -0500

    merge to master

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import { Routing } from "./constants/Routes";
 import Layout from "./containers/Layout";
 
 // This is the entry point for the app.
-ReactDOM.render(
-	<Layout>
-	</Layout>,
-	document.getElementById("root")
-);
+ReactDOM.render(<Layout></Layout>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
