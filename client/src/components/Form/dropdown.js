01fb0d39c34d6a25edbe08a9a014d7ef12e63384
bvo4 <55567877+bvo4@users.noreply.github.com>
2020-03-18 10:46:36 -0500

    Created a form of an exposed drop down menu.  The menu text will change based on what option you click on.

--- client/src/components/Form/dropdown.js
+++ client/src/components/Form/dropdown.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { withStyles } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
 
 //To DO:  Finish passing down states to the menu.  Create an exposed drop down menu.
 
 export default function DropDown() {
 
+	/* Used to determine the CSS of the menu */
 	const useStyles = makeStyles(theme => ({
 		root: {
 			display: 'flex',
@@ -19,10 +19,14 @@ export default function DropDown() {
 		},
 	}));
 
+	//Sets the default text of the menu button
+	const defaultValue = "Menu";
+	var [select, setSelect] = React.useState(defaultValue);
 
-	var [select, setSelect] = React.useState("Menu");
-
+	//Used for CSS styling
 	const classes = useStyles();
+
+	//Used to anchor the menu to the drop down menu button
 	const [anchorEl, setAnchorEl] = React.useState(null);
 
 	const options = [
@@ -32,29 +36,47 @@ export default function DropDown() {
 		'And Hurt you',
 	];
 
-	const handleChange = event => {
-		setSelect("test");
+	/* Change the text of the manu button when this function is called */
+	const handleChange = key => {
+
+		/* Changes the text of the drop down menu button back to the default value */
+		setSelect(defaultValue);
+
+		/* If received parameter does not match one of the options (User clicked outside the button),
+		 * reset the button text back to the default value */
+		for (var i = 0; i < options.length ; i++)
+		{
+			if (options[i] != key);
+
+			else {
+				setSelect(key);
+				break;
+			}
+		}
 	};
 
+	/* Creates and anchors the dropdown menu to the button */
 	const handleClick = event => {
 		setAnchorEl(event.currentTarget);
 	};
 
 	const handleClose = (key) => {
-		alert(key);
 		setAnchorEl(null);
-		handleChange();
+		handleChange(key);
 	};
 
 	return (
 		<div className = "classes">
 			<Button
+				color="primary"
+				variant = "outlined"
 				contentEditable = "false"
 				aria-label="more"
 				aria-controls="long-menu"
 				aria-haspopup="true"
-				onClick={handleClick}>
-				{select}
+				onClick={handleClick}
+				>
+				{select} ▼
       </Button>
 			<Menu
 				id="simple-menu"
@@ -66,8 +88,8 @@ export default function DropDown() {
 			>
 				{options.map(options => (
 					<MenuItem
-						type = "text"
-						
+						type="text"
+
 						onClick={
 							() => handleClose(options)
 						}
