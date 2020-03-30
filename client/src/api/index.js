64d7b6e73af92a4942ed12ff8f4c4206edbc5c8d
w3aseL <noahtemp13@gmail.com>
2020-03-29 23:04:47 -0500

    Setting up cookies

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,6 @@
 import API_HOST from './config'
 import axios from 'axios'
+import cookies from 'js-cookie'
 
 /* API Calling
  * ----------
@@ -9,14 +10,15 @@ import axios from 'axios'
  * for each container. The requests should provide all the data needed in a JSON format. If it doesn't, we will change
  * that.
  */
-
-async function request(endpoint, body, type, authenticated, contentType="application/json") {
+const request = async (endpoint, body, type, authenticated, contentType="application/json") => {
   var headers = {
     'Content-Type': contentType
   }
 
   if (authenticated) {
-    //Setup authentication checks here and add header with token
+    if(!getAccessToken()) await refreshAccessToken()
+
+    headers['Authorization'] = `Bearer ${getAccessToken()}`
   }
   
   var response = await axios({
@@ -29,14 +31,49 @@ async function request(endpoint, body, type, authenticated, contentType="applica
   return response
 }
 
-export default class ChaseYourDreamsAPI {
-  constructor() {
+async function refreshAccessToken() {
+  const refresh = getRefreshToken()
 
-  }
+  if(!refresh) throw new Error("No valid refresh token exists!")
 
-  async testCall() {
-    var response = await request("/", null, "GET", false)
+  var response = await axios({
+    method: "GET",
+    url: API_HOST + "/token/refresh/",
+    headers: {
+      'Content-Type': "application/json",
+      'Authentication': `Bearer ${refresh}`
+    }
+  })
 
-    return response
-  }
+  updateTokens(response.data)
 }
+
+/**
+ * Cookie Handling
+ * ----------------------
+ * For the tokens only, we will store these in a HTTP only cookie (security)
+ * Everything else will be stored in the redux state store
+ */
+const accessCookieName = "access_token"          //We'll make this more secure later
+const refreshCookieName = "refresh_token"        //We'll make this more secure later as well
+
+function getAccessToken() {
+  return cookies.get(accessCookieName)
+}
+
+function getRefreshToken() {
+  return cookies.get(refreshCookieName)
+}
+
+function updateTokens(tokens) {
+  cookies.set(accessCookieName, accessToken, { expires: 1/24 })
+  if(tokens.refresh_token) cookies.set(refreshCookieName, refreshToken, { expires: 365 })
+}
+
+function removeTokens() {
+  cookies.remove(accessCookieName)
+  cookies.remove(refreshCookieName)
+}
+
+/* Exports */
+export { request, updateTokens, removeTokens }
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import rootReducer from './reducers/index';
 
 const loggerMiddleware = createLogger();
 
-export default createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
-);
+const rootReducer = combineReducers({
+
+});
+
+export default createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
