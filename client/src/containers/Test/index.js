dd80d6ab1805674f1bb98dd5e746babd0a0d94b7
bvo4 <55567877+bvo4@users.noreply.github.com>
2020-04-01 22:43:36 -0500

    2 weeks worth of work thrown out the window.  I hate everything about this.  I wasted so much of my time on this and it led to nothing.  I hate this so much. Can I get some rest for one day at the very least.

--- client/src/containers/Test/index.js
+++ client/src/containers/Test/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import styled from "styled-components";
 import theme from "../../constants/theme";
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { green, purple } from '@material-ui/core/colors';
+import { View } from '../../../node_modules/react-native';
+import FormControl from "@material-ui/core/FormControl";
 import clsx from 'clsx';
 import LinkButtons from "../../components/Buttons/LinkButton";
 import StyledButtons from "../../components/Buttons/StyledButton";
@@ -10,71 +14,109 @@ import Textbox from "../../components/Form/textbox";
 import DropDown from "../../components/Form/dropdown";
 import SubmitForm from "../../components/Form/index";
 import OutlinedCard from "../../components/Card/container";
+import BusinessLeader from "../../assets/stockimages/SkyScape.jpg";
 
+const useStyles = makeStyles({
+	root: {
+		minWidth: 275,
+		maxWidth: 300,
+	},
+
+	imagewrapper: {
+		display: 'inline-block',
+		width: '100%',
+		height: 800,
+
+	},
+
+	center: {
+		textAlign: 'center', // <-- the magic
+		fontWeight: 'bold',
+		fontSize: 18,
+		marginTop: 0,
+		color: 'white',
+	},
+
+	button: {
+		color: theme.palette.getContrastText(purple[500]),
+		backgroundColor: '#e53935',
+		'&:hover': {
+			backgroundColor: purple[700],
+		},
+	},
+
+	row: {
+		display: 'flex',
+		flexDirection: 'row',
+		'justify-content': 'space-evenly',
+	},
+
+});
+
+const options = ["Job Titles", "Salary Range", "Location", "Search"];
 
 const Test = props => {
+
+	const classes = useStyles();
+
 	return (
 		<div
 			style={{ overflow: "auto" }}
 		>
-      <LinkButtons
-			  height={100}
-			  width={100}
-			  Link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-			  variant="danger"
-			  size="lg"
-			  text="Link Button">
-		  </LinkButtons>
-
-		  <br />
-		  <br />
-		  
-		  <LinkButtons
-			height={50}
-			width={50}
-			Link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-			variant="success"
-			size="sm"
-			text="Small Button">
-		   </LinkButtons>
+			<FormControl>
+			<img src={BusinessLeader} className={classes.imagewrapper} />
+			text
+			</FormControl>	
+			<h1 className={classes.center}
+			>
+				Employer Page
+			</h1>
 
 			<br />
 			<br />
 
-		  <DropDown
-				color=""				//Might changes this to affect styling later
-				contentEditable="false"
-				controls="long-menu"
-				defaultValue="Menu"
-				haspopup="true"
-				label="more"
-				variant="contained"
-			  options={[
-				  'Never gonna give you up',
-				  'Never gonna let you down',
-				  'Never gonna take a stand',
-				  'And Hurt you',
-			  ]}
-		  >
-		  </DropDown>
+			<h2 className={classes.center}>
+				You are one step closer to finding someone to help build your dream
+			</h2>
+			
+			<FormControl className={classes.row}>
 
-		<OutlinedCard>
-		</OutlinedCard>
+			{options.map(options => (
+			<Button
+					variant="outlined"
+					color="primary"
+					className={clsx(classes.button, classes.row)}
+			>
+						{options}
+			</Button>
 
+				))
+				}
 
-		  <StyledButtons
-			  display='inline-block'						//Changes block display setting
-			  border='3px'									//Works
-			  padding='1rem '								//works
-			  margin='0rem'									//Works
-			  background = 'cornflowerblue'					//Works
-		      color = 'red'									//Does not work for some reason
-			  borderSizeAndColor='2px solid red'			//Works
-			  Link="https://www.youtube.com/watch?v=IEGo41443iI"
-			  text = "Style Button"
-		  >
-		  </StyledButtons>
-    </div>
+			</FormControl>
+
+			<br />
+
+			<h1 className={classes.center}>
+			Preferences
+			</h1>
+
+			<CheckboxApp
+				label="Primary"
+				row="row"
+				className={classes.center}
+				options={[
+					'Qualifications',
+					'Qualifications',
+					'Qualifications',
+					'Qualifications',
+					'Qualifications',
+					'Qualifications',
+				]}
+		>
+			</CheckboxApp>
+
+		</div>
   )
 }
 
