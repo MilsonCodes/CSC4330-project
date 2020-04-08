830b49a2b1f3f110663e8e3d1190e5b290b9a057
leopoldmarx <leopoldmarx@github.com>
2020-04-08 10:36:04 -0500

    Made managers and applicants files

--- client/src/containers/Applications/index.js
+++ client/src/containers/Applications/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import LinkButton from "../../components/Buttons/LinkButton.js"
 
+//class Button extends StyledButton
 
 const UserApplications = props => {
     //TODO: Create user applications page
-
     return (
         <div>
-            <h1>Applications</h1>
+            <h1><font color="white">Applications</font></h1>
+            <LinkButton color="white"
+            	text={`test`}
+            	padding="40"
+            />
         </div>
     );
 }
