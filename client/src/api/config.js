903d86d1b5f087021f1bc62603186f963c577f52
w3aseL <noahtemp13@gmail.com>
2020-04-20 23:39:09 -0500

    Updated IP for backend on frontend

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "chaseyourdreams.noahtemplet.dev")                      //Imagine that. This will likely never happen lol
     backendHost = "https://api.chaseyourdreams.noahtemplet.dev/api"
 else
-    backendHost = "http://ec2-18-222-89-143.us-east-2.compute.amazonaws.com/api"
+    backendHost = "http://ec2-3-22-110-115.us-east-2.compute.amazonaws.com/api"
 
 const API_HOST = backendHost
 
