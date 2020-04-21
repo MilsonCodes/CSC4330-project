a8dda36dd560dd6b555cef03f3af7448764e7706
root <root@ip-172-31-17-210.us-east-2.compute.internal>
2020-04-21 21:22:12 +0000

    save password correctly on register

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -41,7 +41,12 @@ SECRET_KEY = '!0)iqga2mpb-5=vsu_k9ek+&(+a&wlba5$)fzw#5!5@*2qcgtl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-18-222-89-143.us-east-2.compute.amazonaws.com', '18.222.89.143', 'localhost']
+ALLOWED_HOSTS = [
+        'ec2-3-22-110-115.us-east-2.compute.amazonaws.com', 
+        '3.22.110.115', 
+        'localhost',
+        'api.chaseyourdreams.noahtemplet.dev'
+    ]
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
--- client/src/redux/user/constants.js
+++
@@ -1,6 +0,0 @@
-/*  TODO: Start separating data fetching based on data type (i.e. company, application, listing, etc.)  */
-export default {
-  FETCH_USERS_REQUEST: "FETCH_USERS_REQUEST",
-  FETCH_USERS_SUCCESS: "FETCH_USERS_SUCCESS",
-  FETCH_USERS_ERROR: "FETCH_USERS_ERROR"
-}
